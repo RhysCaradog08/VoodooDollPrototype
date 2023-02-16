64001b18d809b50972dff8d7d9d1f67e8709f923
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2023-02-16 15:41:44 +0000

    Iterated Target selection for Needle Throw
    Needle will now auto fire towards a target in range but cannot fire a second needle at a target with a needle already in it.

--- Voodoo Doll Prototype/Assets/Scripts/Needle.cs
+++ Voodoo Doll Prototype/Assets/Scripts/Needle.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Needle : MonoBehaviour
 {
     public Rigidbody rb;
     LineRenderer line;
 
+    [Header("Throw_RecallNeedle")]
     public Transform throwPoint, tetherPoint, needleHolder;
     Quaternion needleStartRotation;
-    [SerializeField] float throwDistance, throwForce, recallSpeed, recallDistance, startTime;
+    [SerializeField] float throwForce, recallSpeed, recallDistance, startTime;
     public LayerMask ignoreLayer;
-
     public bool needleThrown, recallingNeedle;
+
+    [Header("FindTargets")]
+    [SerializeField] Transform target;
+    [SerializeField] List<Transform> targetLocations = new List<Transform>();
+    Collider[] hitColliders;
+    [SerializeField] float targetRadius;
 
     private void Awake()
     {
@@ -60,6 +67,11 @@ public class Needle : MonoBehaviour
             line.SetPosition(1, tetherPoint.position);
         }
         else line.enabled = false;
+
+        foreach(Transform t in targetLocations)
+        {
+            Debug.DrawLine(throwPoint.position, t.position, Color.red);
+        }
     }
 
     public void ThrowNeedle()
@@ -68,14 +80,12 @@ public class Needle : MonoBehaviour
         transform.rotation = Quaternion.Euler(90, 0, 0);
         rb.isKinematic = false;
 
-        RaycastHit hit;
-        if (Physics.Raycast(throwPoint.position, throwPoint.TransformDirection(Vector3.forward), out hit, throwDistance, ~ignoreLayer))
-        {
-            Debug.Log("Raycast Hit");
-            Debug.DrawRay(throwPoint.position, throwPoint.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Vector3 throwDirection = hit.point - transform.position;
 
-            rb.AddForce(throwDirection * throwForce, ForceMode.Impulse);
+        if (target != null)
+        {
+            Vector3 throwDirection = target.position - transform.position;
+
+            rb.AddForce(throwDirection * 5, ForceMode.Impulse);
         }
         else
         {
@@ -88,6 +98,9 @@ public class Needle : MonoBehaviour
 
     public void RecallNeedle()
     {
+        targetLocations.Clear();
+        target = null;
+        transform.parent = null;
         rb.isKinematic = false;
 
         float distanceCovered = (Time.time - startTime) * recallSpeed;
@@ -113,5 +126,51 @@ public class Needle : MonoBehaviour
         {
             rb.isKinematic = true;
         }
+
+        if(collision.gameObject.tag == "Target")
+        {
+            transform.parent = collision.transform;
+        }
+    }
+
+    public void FindTarget()
+    {
+        hitColliders = Physics.OverlapSphere(transform.position, targetRadius);
+
+        foreach (Collider col in hitColliders)
+        {
+            if (col.tag == "Target")
+            {
+                if (!targetLocations.Contains(col.transform) && col.transform.childCount < 1)
+                {
+                    targetLocations.Add(col.transform);
+                }
+            }
+        }
+    }
+
+    public void FindClosestTarget()
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.root.position;
+        foreach (Transform potentialTarget in targetLocations)
+        {
+            Vector3 directionToTarget = potentialTarget.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        target = bestTarget;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, targetRadius);
     }
 }
--- Voodoo Doll Prototype/Assets/Scripts/NeedleController.cs
+++ Voodoo Doll Prototype/Assets/Scripts/NeedleController.cs
@@ -21,6 +21,8 @@ public class NeedleController : MonoBehaviour
         {
             if(!needleScript1.needleThrown)
             {
+                needleScript1.FindTarget();
+                needleScript1.FindClosestTarget();
                 needleScript1.ThrowNeedle();
             }
             else if (needleScript1.needleThrown && needleScript1.rb.isKinematic)
@@ -33,6 +35,8 @@ public class NeedleController : MonoBehaviour
         {
             if (!needleScript2.needleThrown)
             {
+                needleScript2.FindTarget();
+                needleScript2.FindClosestTarget();
                 needleScript2.ThrowNeedle();
             }
             else if (needleScript2.needleThrown && needleScript2.rb.isKinematic)
